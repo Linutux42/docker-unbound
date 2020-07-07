fec813768c31e6c12217670aebe11166a7edb569
Maxence 'Linutux' POULAIN <linutux42@users.noreply.github.com>
2020-07-07 11:34:20 +0200

    Cleanup image since it's not running in k3s anymore

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,12 @@
 FROM debian:buster-slim
 
 COPY unbound-entrypoint.sh /
-COPY unbound-refresher.sh /
 
 RUN apt-get update \
-  && apt-get install -y unbound inotify-tools curl cron \
+  && apt-get install -y unbound curl \
   && rm -rf /var/lib/apt/lists/* \
-  && rm -rf /etc/unbound/unbound_* \
-  && rm -rf /etc/cron.daily \
-  && echo "00 05 * * * root /bin/bash /unbound-refresher.sh" >/etc/cron.d/unbound-ads-refresh
+  && rm -rf /etc/unbound/* \
 
-VOLUME /etc/unbound
+VOLUME /etc/unbound/
 
-ENTRYPOINT [ "sh", "/unbound-entrypoint.sh" ]
+ENTRYPOINT [ "/bin/bash", "/unbound-entrypoint.sh" ]
--- unbound-entrypoint.sh
+++ unbound-entrypoint.sh
@@ -1,12 +1,97 @@
-#!/bin/sh
-set -m
+#!/bin/bash
+#
+# Using blacklist from pi-hole project https://github.com/pi-hole/
+# to enable AD blocking in unbound(8)
+#
+PATH="/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin"
 
-/bin/bash /unbound-refresher.sh
+# Available blocklists - comment line to disable blocklist
+_disconad="https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt"
+_discontrack="https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt"
+_hostfiles="https://hosts-file.net/ad_servers.txt"
+_malwaredom="https://mirror1.malwaredomains.com/files/justdomains"
+_stevenblack="https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
+_zeustracker="https://zeustracker.abuse.ch/blocklist.php?download=domainblocklist"
 
-service cron start
+# Root hints from internic
+_roothints="https://www.internic.net/domain/named.root"
 
-/usr/sbin/unbound -v -c /etc/unbound/unbound.conf
+# Global variables
+_tmpfile="$(mktemp)" && echo '' > $_tmpfile
+_basedir="/etc/unbound"
+_unboundconf="${_basedir}/unbound.conf.d/unbound-adhosts.conf"
 
-while inotifywait --event close_write,moved_to,create,modify /etc/unbound/;
-  do kill -HUP `cat /var/run/unbound/unbound.pid`;
-done
+function _print_status {
+  if [ "${1}" -eq "0" ]; then
+    echo "OK"
+  else
+    echo "FAILED"
+  fi
+}
+
+# Remove comments from blocklist
+function simpleParse {
+  echo "Downloading and parsing ${1} ... "
+  curl -s $1 | \
+  sed -e 's/#.*$//' -e '/^[[:space:]]*$/d' >> $2
+  _print_status "${?}"
+}
+
+# Make sure folder unbound.conf.d exists
+[[ ! -d "${_basedir}/unbound.conf.d" ]] && mkdir ${_basedir}/unbound.conf.d
+
+# Parse MalwareDom
+[[ -n ${_malwaredom+x} ]] && simpleParse $_malwaredom $_tmpfile
+
+# Parse ZeusTracker
+[[ -n ${_zeustracker+x} ]] && simpleParse $_zeustracker $_tmpfile
+
+# Parse DisconTrack
+[[ -n ${_discontrack+x} ]] && simpleParse $_discontrack $_tmpfile
+
+# Parse DisconAD
+[[ -n ${_disconad+x} ]] &&  simpleParse $_disconad $_tmpfile
+
+# Parse StevenBlack
+[[ -n ${_stevenblack+x} ]] && \
+  echo "Downloading and parsing ${1} ... " && \
+  curl -s $_stevenblack | \
+  sed -n '/Start/,$p' | \
+  sed -e 's/#.*$//' -e '/^[[:space:]]*$/d' | \
+  awk '/^0.0.0.0/ { print $2 }' >> $_tmpfile && \
+  _print_status "${?}"
+
+# Parse hpHosts
+[[ -n ${_hostfiles+x} ]] && \
+  echo "Downloading and parsing ${1} ... " && \
+  curl -s $_hostfiles | \
+  sed -n '/START/,$p' | tr -d '^M$' | \
+  sed -e 's/#.*$//' -e '/^[[:space:]]*$/d' -e 's/$//' | \
+  awk '/^127.0.0.1/ { print $2 }' >> $_tmpfile && \
+  _print_status "${?}"
+
+# Create unbound(8) local zone file
+echo "Merging ... "
+echo 'server:' > $_unboundconf && \
+  sort -fu $_tmpfile | \
+  grep -v "^[[:space:]]*$" | \
+awk '{
+print "local-zone: \"" $1 "\" static"
+}' >> $_unboundconf && rm -f $_tmpfile
+_print_status "${?}"
+
+# Download latest root.hints
+echo -n "Downloading root.hints from internic ... "
+curl -so ${_basedir}/root.hints $_roothints
+_print_status "${?}"
+
+# Update DNSSEC root key
+echo -n "Downloading root anchors ... "
+unbound-anchor -v
+_print_status "${?}"
+
+# Start unbound
+/usr/sbin/unbound -d -v -c /etc/unbound/unbound.conf
+
+exit ${?}
+#EOF
